a2d17d4e71f848d3c9e4870a4d578ccb80ce0d62
Vitor Nunes <stecks10@gmail.com>
2021-12-22 18:02:51 -0300

    complet routes use, put, get, delet

--- authentication-api/src/index.ts
+++ authentication-api/src/index.ts
@@ -1,10 +1,15 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'sucesso' });
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(usersRoute);
+
+app.use(statusRoute);
 
 app.listen(3000, () => {
   console.log('Aplicação executando na porta 3000');
